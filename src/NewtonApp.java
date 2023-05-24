64befa35b96c4aab32e151cfe0da05770dabd79e
tejb96 <104933640+tejb96@users.noreply.github.com>
2023-05-24 17:56:46 -0600

    feat: implementation of UI dialogues

--- src/NewtonApp.java
+++ src/NewtonApp.java
@@ -4,10 +4,36 @@
  *
  */
 public class NewtonApp {
+	
+	private InteractiveCLI cli;
+	
+	public NewtonApp() {
+		this.cli = new InteractiveCLI();
+	}
+	
+	public void run() {
+		cli.display("*** Calculate Square Root with Newton ***");
+		
+		cli.prompt("Enter a positive number, e.g. 25.3:");
+		double N = cli.getKeyboardDouble();
+		
+		cli.prompt("Enter a maximum error, e.g. 0.001:");
+		double maxErr = cli.getKeyboardDouble();
+		
+		cli.prompt("Enter an initial guess, e.g. 12.15:");
+		double initialGuess = cli.getKeyboardDouble();
+		
+		NewtonSolver newtonSolve = new NewtonSolver(N, maxErr, initialGuess);
+		
+		cli.display("The square root of " + N + " is " + newtonSolve.sqrt());
+		cli.display("Computed in " + newtonSolve.getIterations() + " iterations");
+
+	}
 
 	public static void main(String[] args) {
 		
-
+		NewtonApp app = new NewtonApp();
+		app.run();
 	}
 
 }
--- src/NewtonSolver.java
+++ src/NewtonSolver.java
@@ -4,5 +4,33 @@
  *
  */
 public class NewtonSolver {
+	
+	private double N;
+	private double maxError;
+	private int iter = 0;
+	private double guess;
+
+	
+	public NewtonSolver(double N, double maxError, double guess) {
+		this.N = N;		
+		this.maxError = maxError;
+		this.guess = guess;
+	}
+	
+	public double sqrt() {
+		double sqrtCalc = 0;
+		double err;
+		do {
+			sqrtCalc = ((N / guess) + guess) / 2;
+			err = guess - sqrtCalc;
+			guess = sqrtCalc;
+			iter++;
+		} while (err > maxError);
+		return sqrtCalc;
+	}
+	
+	public int getIterations() {
+		return iter;
+	}
 
 }
