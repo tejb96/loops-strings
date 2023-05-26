13b9a53a3cac7d59d05c1b294dbc5e7ba7e8c64d
tejb96 <104933640+tejb96@users.noreply.github.com>
2023-05-26 13:22:34 -0600

    feat: implementation of SecretWord class

--- src/SecretWord.java
+++ src/SecretWord.java
@@ -1,15 +1,53 @@
 
 /**
- * @author 
+ * @author Tejpreet Bal
  *
  */
 public class SecretWord {
 	private String secretWord;
 	private String displayWord;
 	
-	public SecretWord() {
+	public SecretWord() { 
 		secretWord = "food";
+		displayWord = replaceChars(secretWord, '*');
 		
+	}
+	
+	public String replaceChars(String secretWord, char replacer){		
+		for(char c='A'; c<='z'; c++) {
+			secretWord = secretWord.replace(c, replacer);
+		}
+		
+		return secretWord;
+	}
+	
+	public void replaceChars(String s1, String s2,  char ch){	
+		
+		StringBuilder sb = new StringBuilder(displayWord);		
+		for(int index=0; index <= secretWord.length()-1; index++) {
+			if(secretWord.charAt(index) == ch) {
+				sb.setCharAt(index, ch);
+			}
+		}
+		displayWord = sb.toString();
+	}
+	
+	public String getDisplayedWord() {
+		return displayWord;
+	}
+	
+	public Boolean makeGuess(char ch) {
+		int letterIndex = 0;
+		letterIndex = secretWord.indexOf(ch);
+		if (letterIndex == -1) {
+			return false;
+		}
+		
+		else {
+			
+			replaceChars(secretWord, displayWord, ch);
+			return true;
+		}			
 	}
 
 }
